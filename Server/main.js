654c712a879b2c37785479cb2e4be9b1ed1d0461
Mohammad Saeed Roohbakhsh <roohbakhsh.saeed81@gmail.com>
2023-09-14 12:36:02 +0330

    Initial server configuration

---
+++ Server/main.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
+
+const {PORT} = require("./config/server")
+const routes = require("./routes")
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+app.use(routes)
+
+app.listen(PORT, function () {
+    console.log("Started listening on port " + PORT)
+})
