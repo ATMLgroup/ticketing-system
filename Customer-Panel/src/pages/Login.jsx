e0e6b159254643e267be7049af1154f0f2db2878
Mohammad Saeed Roohbakhsh <roohbakhsh.saeed81@gmail.com>
2023-09-15 18:50:52 +0330

    route guard for login and dashboard page - customer panel

--- Customer-Panel/src/pages/Login.jsx
+++ Customer-Panel/src/pages/Login.jsx
@@ -1,16 +1,21 @@
 import {LoginLayout} from "../layout/login/LoginLayout";
 import Cookie from "js-cookie"
 import jwtDecode from "jwt-decode"
+import {useNavigate} from "react-router-dom"
+import {useEffect} from "react";
 
 export const Login = () => {
-    const token = Cookie.get('token')
-    if(token) {
-        const decodeToken = jwtDecode(token)
-        console.log(decodeToken)
-    }
-    return (
-        <>
-            <LoginLayout/>
-        </>
-    )
+    const navigate = useNavigate()
+    useEffect(() => {
+        try {
+            const token = Cookie.get('token')
+            const {email} = jwtDecode(token)
+            if (email) {
+                navigate("/dashboard")
+            }
+        } catch (err) {
+            Cookie.remove('token')
+        }
+    }, []);
+    return (<LoginLayout/>)
 }
