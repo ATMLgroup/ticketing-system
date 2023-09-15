fba5fbf0c32bbcf4eabb689ffd4382f26b2f1c8d
Mohammad Saeed Roohbakhsh <roohbakhsh.saeed81@gmail.com>
2023-09-15 16:56:57 +0330

    route guard for login and dashboard page - customer panel

--- Customer-Panel/src/pages/Login.jsx
+++ Customer-Panel/src/pages/Login.jsx
@@ -1,6 +1,13 @@
 import {LoginLayout} from "../layout/login/LoginLayout";
+import Cookie from "js-cookie"
+import jwtDecode from "jwt-decode"
 
 export const Login = () => {
+    const token = Cookie.get('token')
+    if(token) {
+        const decodeToken = jwtDecode(token)
+        console.log(decodeToken)
+    }
     return (
         <>
             <LoginLayout/>
