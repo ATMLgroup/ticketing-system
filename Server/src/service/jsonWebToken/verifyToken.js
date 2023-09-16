2f137d3b9c7bd5862c9fd5bb7eae06ee5b03fbfa
Mohammad Saeed Roohbakhsh <roohbakhsh.saeed81@gmail.com>
2023-09-16 17:25:36 +0330

    update verify token in service/jsonWebToken

--- Server/src/service/jsonWebToken/verifyToken.js
+++ Server/src/service/jsonWebToken/verifyToken.js
@@ -6,18 +6,19 @@ const {join} = require("path")
  * @module
  * @description This module is responsible for verifying token
  * @param {String}token return token for authentication
- * @returns {{status: boolean, token: string}}
+ * @returns {{status: boolean, email: string}}
  */
 module.exports = async (token) => {
 
     let response = {
         status: true,
-        response: ""
+        email: ""
     }
 
     try {
         const publicKey = readFileSync(join('cert', 'jwt', 'rsa.public.key'), 'utf8')
-        return jwt.verify(token, publicKey, { algorithm: 'RS256'});
+        const data = await jwt.verify(token, publicKey, {algorithm: 'RS256'});
+        response.email = data.email
     } catch (err) {
         console.log(err)
         response.status = false
