a635f2803ab960783d13983668ffcf2321b106fb
Mohammad Saeed Roohbakhsh <roohbakhsh.saeed81@gmail.com>
2023-09-22 15:15:14 +0330

    add /customers/reply route

--- Server/src/routes/customers.js
+++ Server/src/routes/customers.js
@@ -6,18 +6,21 @@ const signUpMiddleware = require("../middlewares/customers/signUp.middleware")
 const signInMiddleware = require("../middlewares/customers/signIn.middleware")
 const changePasswordMiddleware = require("../middlewares/customers/changePassowrd.middleware")
 const addMiddleware = require("../middlewares/customers/add.middleware")
+const replyMiddleware = require("../middlewares/customers/reply.middleware")
 
 const signUpController = require("../controllers/customers/signUp.controller")
 const signInController = require("../controllers/customers/signIn.controller")
 const changePasswordController = require("../controllers/customers/changePassword.controller")
 const addController = require("../controllers/customers/add.controller")
 const ticketsController = require("../controllers/customers/tickets.controller")
+const replyController = require("../controllers/customers/reply.controller")
 
 router.post("/signup", signUpMiddleware, signUpController)
 router.post("/signin", signInMiddleware, signInController)
 router.use(authenticationMiddleware)
 router.post("/change_password", changePasswordMiddleware, changePasswordController)
 router.post("/add", addMiddleware, addController)
+router.post("/reply", replyMiddleware, replyController)
 router.get("/tickets", ticketsController)
 
 module.exports = router
