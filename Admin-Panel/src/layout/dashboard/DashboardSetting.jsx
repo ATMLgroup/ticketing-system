ccb045cd3067722d0fda22e9104132090f7ef545
Mohammad Saeed Roohbakhsh <roohbakhsh.saeed81@gmail.com>
2023-10-05 21:47:40 +0330

    add new user modal

--- Admin-Panel/src/layout/dashboard/DashboardSetting.jsx
+++ Admin-Panel/src/layout/dashboard/DashboardSetting.jsx
@@ -1,9 +1,11 @@
 import {
-    BoldOutlined,
+    UserOutlined,
     ExclamationCircleFilled,
     LockOutlined,
     PoweroffOutlined,
-    SettingOutlined
+    SettingOutlined,
+    PhoneOutlined,
+    MailOutlined
 } from "@ant-design/icons";
 import {App, Button, Col, FloatButton, Input, Modal, Row, Tooltip} from "antd";
 import {useState} from "react";
@@ -18,8 +20,22 @@ export const DashboardSetting = () => {
     const navigate = useNavigate()
 
     const [changePasswordStatus, setChangePasswordStatus] = useState(false);
+    const [createNewUserStatus, setCreateNewUserStatus] = useState(false);
     const [currentPassword, serCurrentPassword] = useState("")
     const [newPassword, serNewPassword] = useState("")
+    const [newFirstname, setNewFirstname] = useState("")
+    const [newLastname, setNewLastname] = useState("")
+    const [newEmail, setNewEmail] = useState("")
+    const [newPhone, setNewPhone] = useState("")
+    const [newUserPassword, setNewUserPassword] = useState("")
+
+    const inputStyle = {
+        width: "90%",
+        marginTop: "10px",
+        display: "flex",
+        marginRight: "auto",
+        marginLeft: "auto"
+    }
 
     /**
      * @description Change the password handler
@@ -31,6 +47,24 @@ export const DashboardSetting = () => {
         })
         if (typeof data === 'object' && !Array.isArray(data) && Object.keys(data).length !== 0) {
             message.success("change password successfully")
+        } else {
+            message.error(error.response.data.detail)
+        }
+    }
+
+    /**
+     * @description Create new user handler
+     */
+    const createNewUserModalHandler = async () => {
+        const {data, error} = await sendRequest("admin/new_user", "POST", {
+            firstName: newFirstname,
+            lastName: newLastname,
+            email: newEmail,
+            password: newUserPassword,
+            phoneNumber: newPhone
+        })
+        if (typeof data === 'object' && !Array.isArray(data) && Object.keys(data).length !== 0) {
+            message.success("Create new user successfully")
         } else {
             message.error(error.response.data.detail)
         }
@@ -101,6 +135,83 @@ export const DashboardSetting = () => {
                     </Col>
                 </Row>
             </Modal>
+            <Modal
+                centered
+                width={"85%"}
+                closable={false}
+                title={"Create new user"}
+                open={createNewUserStatus}
+                footer={(_, {OkBtn, CancelBtn}) => (
+                    <>
+                        <div style={{marginTop: "50px"}}>
+                            <Button
+                                onClick={() => setCreateNewUserStatus(false)}
+                                type={"dashed"}>Cancel</Button>
+                            <Button
+                                disabled={!(
+                                    newFirstname.length > 0 &&
+                                    newLastname.length > 0 &&
+                                    newPhone.length > 0 &&
+                                    newEmail.length > 0 &&
+                                    newUserPassword.length > 0
+                                )}
+                                onClick={() => createNewUserModalHandler()}
+                                type={"primary"}>
+                                Create user
+                            </Button>
+                        </div>
+                    </>
+                )}>
+                <Row
+                    justify={"space-around"}
+                    align={"middle"}>
+                    <Col span={8}>
+                        <Input
+                            onChange={(e) => setNewFirstname(e.target.value)}
+                            value={newFirstname}
+                            placeholder={"Firstname"}
+                            size={"large"}
+                            style={inputStyle}
+                            prefix={<UserOutlined style={{color: "rgba(0, 0, 0, 0.45)"}}/>}/>
+                    </Col>
+                    <Col span={8}>
+                        <Input
+                            onChange={(e) => setNewLastname(e.target.value)}
+                            value={newLastname}
+                            placeholder={"Lastname"}
+                            size={"large"}
+                            style={inputStyle}
+                            prefix={<UserOutlined style={{color: "rgba(0, 0, 0, 0.45)"}}/>}/>
+                    </Col>
+                    <Col span={8}>
+                        <Input
+                            onChange={(e) => setNewPhone(e.target.value)}
+                            value={newPhone}
+                            placeholder={"Phone number"}
+                            size={"large"}
+                            style={inputStyle}
+                            prefix={<PhoneOutlined style={{color: "rgba(0, 0, 0, 0.45)"}}/>}/>
+                    </Col>
+                    <Col span={12}>
+                        <Input
+                            onChange={(e) => setNewEmail(e.target.value)}
+                            value={newEmail}
+                            placeholder={"Email"}
+                            size={"large"}
+                            style={{...inputStyle, width: "94%"}}
+                            prefix={<MailOutlined style={{color: "rgba(0, 0, 0, 0.45)"}}/>}/>
+                    </Col>
+                    <Col span={12}>
+                        <Input.Password
+                            onChange={(e) => setNewUserPassword(e.target.value)}
+                            value={newUserPassword}
+                            placeholder={"password"}
+                            size={"large"}
+                            style={{...inputStyle, width: "94%"}}
+                            prefix={<LockOutlined style={{color: "rgba(0, 0, 0, 0.45)"}}/>}/>
+                    </Col>
+                </Row>
+            </Modal>
             <FloatButton.Group
                 trigger="hover"
                 type="default"
@@ -121,6 +232,13 @@ export const DashboardSetting = () => {
                     <FloatButton
                         onClick={() => setChangePasswordStatus(true)}
                         icon={<LockOutlined/>}/>
+                </Tooltip>
+                <Tooltip
+                    placement="leftTop"
+                    title={"Create new user"}>
+                    <FloatButton
+                        onClick={() => setCreateNewUserStatus(true)}
+                        icon={<UserOutlined/>}/>
                 </Tooltip>
             </FloatButton.Group>
         </>
